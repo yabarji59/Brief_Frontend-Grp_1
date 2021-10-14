bad320d0f2608482965714f3363ffed12e0f3ec2
Virginie <virginie.finez@laposte.net>
2021-10-14 23:01:49 +0200

    Affichage du JSON

--- myapp/src/app/users-list/users-list.component.ts
+++ myapp/src/app/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
+import { User } from '../user-interface';
 import personsJson from '../../assets/data/JSON_Data_User.json';
 
 @Component({
@@ -6,34 +8,35 @@ import personsJson from '../../assets/data/JSON_Data_User.json';
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
+
 export class UsersListComponent implements OnInit {
   // Recuperer l'objet 'results' du fichier JSON
   personsJson: any = personsJson.results;
   // Créer un tableau d'utilisateurs
-  // users: User[];
+  users: User[];
 
   constructor() { }
 
   ngOnInit(): void {
     // Relier les données du JSON à un utilisateur
-    // this.users = <User[]>this.personsJson;
+    this.users = < User[] > this.personsJson;
     // Vérifier en console
-    // console.log(this.users);
+    console.log(this.users);
   }
 
   // Obtenir l'icône selon l'âge
-  // getImageByAge(age: number): string {
+  getImageByAge(age: number): string {
     // Si moins de 40 ans
-    // if (age < 40) {
-    //   return 'rookie.svg';
+    if (age < 40) {
+      return 'rookie.svg';
     // Si entre 40 et 60 ans
-    // } else if (age <= 60 && age >= 40) {
-    //   return 'advance.svg';
+    } else if (age <= 60 && age >= 40) {
+      return 'advance.svg';
     // Si plus de 60 ans
-    // } else if (age > 60) {
-    //   return 'expert.svg';
-    // }
-    // return '';
-  // }
+    } else if (age > 60) {
+      return 'expert.svg';
+    }
+    return '';
+  }
 
 }
