905ce2b25f19852f1faee46b7d5ba594c3b54f12
Céccile <simoncecilepro@outlook.fr>
2021-10-14 16:24:50 +0200

    Roots

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersListComponent } from './users-list/usersListComponent';
+import { UserDetailsComponent } from './user-details/userDetailsComponent';
 
 const routes: Routes = [
-//   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+//   ancien projet { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 //   { path: 'dashbord', component: DashboardComponent },
 //   { path: 'detail/:username', component: UtilisateurDetailComponent },
 //   { path: 'utilisateurs', component: UtilisateursComponent }
+
+  // 1ère route - 
+  // component " users-list" 
+
+  { path: 'usersList', component: UsersListComponent },
+    // 2ème route - 
+  // component " user-details "
+  { path: 'userDetails', component: UserDetailsComponent },
+  /** localhost: 4200  */
+  { path: '', component.UsersListComponent }
+
 ];
 
 @NgModule({
